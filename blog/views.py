d6087b3a49e7c608e53b0b0bca69ae327e5944c9
samul95@mail.ru <samul95@mail.ru>
2019-01-22 00:44:09 +0300

    add page work

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post #Точка перед models означает текущую директорию или текущее приложение
 from django.utils import timezone
 from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -12,5 +13,14 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
